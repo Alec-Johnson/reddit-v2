a9c7354535d96c9879f0a5320b37aca5493c479a
Alec Johnson <akj3211@gmail.com>
2022-01-16 14:22:54 -0600

    Logout route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -107,9 +107,25 @@ const me = async (req: Request, res: Response) => {
 	}
 };
 
+const logout = (_: Request, res: Response) => {
+	res.set(
+		"Set-Cookie",
+		cookie.serialize("token", "", {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			expires: new Date(0), // Cookie will expire immediately
+			path: "/",
+		})
+	);
+
+	return res.status(200).json({ success: true });
+};
+
 const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", me);
+router.get("/logout", logout);
 
 export default router;
