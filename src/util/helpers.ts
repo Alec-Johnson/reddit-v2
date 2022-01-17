034679db6a1f79567ac9645f98d95a861bf50968
Alec Johnson <akj3211@gmail.com>
2022-01-17 11:55:32 -0600

    makeSlug helper function for our posts

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,3 +1,4 @@
+// Thanks to https://stackoverflow.com/a/1349426/9826177
 export function makeId(length: number) {
 	let result = "";
 	const characters =
@@ -7,4 +8,26 @@ export function makeId(length: number) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	}
 	return result;
+}
+
+// https://gist.github.com/codeguy/6684588#gistcomment-2759673
+export function makeSlug(str: string) {
+	str = str.trim();
+	str = str.toLowerCase();
+
+	// remove accents, swap ñ for n, etc
+	const from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+	const to = "aaaaaaeeeeiiiioooouuuunc------";
+
+	for (let i = 0, l = from.length; i < l; i++) {
+		str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+	}
+
+	return str
+		.replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+		.replace(/\s+/g, "-") // collapse whitespace and replace by -
+		.replace(/-+/g, "-") // collapse dashes
+		.replace(/^-+/, "") // trim - from start of text
+		.replace(/-+$/, "") // trim - from end of text
+		.replace(/-/g, "_"); // Reddit uses the _ as a seperator
 }
