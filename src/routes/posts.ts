ba99baa9ba30baf536d24cb17a6ca377a8d24f4c
Alec Johnson <akj3211@gmail.com>
2022-01-17 11:47:38 -0600

    Start building posts route

---
+++ src/routes/posts.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from "express";
+import { Post } from "../entities/Post";
+
+import auth from "../middleware/auth";
+
+const createPost = async (req: Request, res: Response) => {
+	const { title, body, sub } = req.body;
+
+	const author = res.locals.user;
+
+	if (title.trim() === "") {
+		return res.status(400).json({ title: "Title must not be empty" });
+	}
+
+	try {
+		// Todo: find sub
+
+		const post = new Post({ title, body, author, subName: sub });
+		await post.save();
+
+		return res.json(post);
+	} catch (err) {
+		console.log(err);
+		// Already authenticated at this point so any error is a 500
+		return res.status(500).json({ error: "Something went wrong" });
+	}
+};
+
+const router = Router();
+router.post("/", auth, createPost);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,12 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 import trim from "./middleware/trim";
+
 import authRoutes from "./routes/UserRouter";
+import postRoutes from "./routes/posts";
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("dev")); // Dev logging, nice colors
@@ -19,9 +22,10 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Hollow world"));
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
-app.listen(5000, async () => {
-	console.log("Server started on port 5000");
+app.listen(PORT, async () => {
+	console.log(`Server started on port ${PORT}`);
 
 	try {
 		await createConnection();
