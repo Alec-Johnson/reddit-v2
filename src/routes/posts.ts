f58025dcbee14fae7b069eb4373beec06cd3c743
Alec Johnson <akj3211@gmail.com>
2022-01-17 14:15:35 -0600

    Comment route and handler

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import Comment from "../entities/Comment";
 
 import Post from "../entities/Post";
 import Sub from "../entities/Sub";
@@ -57,9 +58,31 @@ const getPost = async (req: Request, res: Response) => {
 	}
 };
 
+const commentOnPost = async (req: Request, res: Response) => {
+	const { identifier, slug } = req.params;
+	const body = req.body.body;
+
+	try {
+		const post = await Post.findOneOrFail({ identifier, slug });
+
+		const comment = new Comment({
+			body,
+			author: res.locals.user, // auth middleware gives access to user
+			post,
+		});
+
+		await comment.save();
+		return res.json(comment);
+	} catch (err) {
+		console.log(err);
+		return res.status(404).json({ error: "Post not found" });
+	}
+};
+
 const router = Router();
 router.post("/", auth, createPost);
 router.get("/", getPosts);
 router.get("/:identifier/:slug", getPost);
+router.post("/:identifier/:slug/comments", auth, commentOnPost);
 
 export default router;
