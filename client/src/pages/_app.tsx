201e8538c418f69b19e7e658a5590f62e9f2c0f2
Alec Johnson <akj3211@gmail.com>
2022-01-17 14:32:58 -0600

    Initialize frontend with create-next-app and tailwindcss

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Register() {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
