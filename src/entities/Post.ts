86767f0712f07f4ca65397a57c8178bfd83dfc33
Alec Johnson <akj3211@gmail.com>
2022-01-17 14:15:56 -0600

    Create comment entity, add relation to post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,8 +5,10 @@ import {
 	ManyToOne,
 	JoinColumn,
 	BeforeInsert,
+	OneToMany,
 } from "typeorm";
 import { makeId, makeSlug } from "../util/helpers";
+import Comment from "./Comment";
 
 import Entity from "./Entity";
 import Sub from "./Sub";
@@ -43,6 +45,9 @@ export default class Post extends Entity {
 	@ManyToOne(() => Sub, (sub) => sub.posts)
 	@JoinColumn({ name: "subName", referencedColumnName: "name" })
 	sub: Sub;
+
+	@OneToMany(() => Comment, (comment) => comment.post)
+	comments: Comment[];
 
 	@BeforeInsert()
 	makeIdAndSlug() {
