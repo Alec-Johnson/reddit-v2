edfb8da2014a66395fd43e72614146cda194622f
Alec Johnson <akj3211@gmail.com>
2022-01-25 12:01:12 -0600

    Add Vote route

---
+++ src/routes/misc.ts
@@ -0,0 +1,68 @@
+// Collection of route for misc purposes, not specfific to posts or users etc
+import { Request, Response, Router } from "express";
+import Comment from "../entities/Comment";
+import Post from "../entities/Post";
+import User from "../entities/User";
+import Vote from "../entities/Vote";
+import auth from "../middleware/auth";
+
+const vote = async (req: Request, res: Response) => {
+	const { identifier, slug, commentIdentifier, value } = req.body;
+
+	// Validate vote value
+	if (![-1, 0, 1].includes(value)) {
+		return res.status(400).json({ value: "Value must be -1, 0, or 1" });
+	}
+
+	try {
+		const author: User = res.locals.user;
+		let post = await Post.findOneOrFail({ identifier, slug });
+		let vote: Vote | undefined;
+		let comment: Comment | undefined;
+
+		if (commentIdentifier) {
+			// if commentIdentifier -> find vote by comment
+			comment = await Comment.findOneOrFail({ identifier: commentIdentifier });
+			vote = await Vote.findOne({ author, comment });
+		} else {
+			// if no commentIdentifier -> find vote by post
+			vote = await Vote.findOne({ author, post });
+		}
+
+		if (!vote && value === 0) {
+			// if no vote and value is 0 -> throw error
+			return res.status(404).json({ error: "Vote not found" });
+		} else if (!vote) {
+			// if no vote -> create new vote
+			vote = new Vote({ author, value });
+			if (comment) vote.comment = comment;
+			else vote.post = post;
+			await vote.save();
+		} else if (value === 0) {
+			// If vote exists and value = 0, delete vote
+			await vote.remove();
+		} else if (vote.value !== value) {
+			// If vote exists and value is different, update vote
+			vote.value = value;
+			await vote.save();
+		}
+
+		// Return vote
+		post = await Post.findOneOrFail(
+			{ identifier, slug },
+			{ relations: ["comments", "comments.votes", "sub", "votes"] }
+		);
+		post.setUserVote(author);
+		post.comments.forEach((c) => c.setUserVote(author));
+
+		return res.json(post);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ error: "Something went wrong" }); // No need for better client side error handling
+	}
+};
+
+const router = Router();
+router.post("/vote", auth, vote);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,10 @@ dotenv.config();
 
 import trim from "./middleware/trim";
 
-import authRoutes from "./routes/UserRouter";
+import authRoutes from "./routes/auth";
 import postRoutes from "./routes/posts";
 import subRoutes from "./routes/subs";
+import miscRoutes from "./routes/misc";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -33,6 +34,7 @@ app.get("/", (_, res) => res.send("Hollow world"));
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/subs", subRoutes);
+app.use("/api/misc", miscRoutes);
 
 app.listen(PORT, async () => {
 	console.log(`Server started on port ${PORT}`);
