ee0c0e82858cc3e2e95f2e554c6e5bd60dc270cf
Alec Johnson <akj3211@gmail.com>
2022-01-27 17:47:23 -0600

    Create 404 page

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+  <section className="flex flex-col items-center">
+    <h1 className="mt-10 mb-4 text-5xl text-gray-800">
+      404: Page not found
+    </h1>
+    
+    <Link href='/'>
+      <a className="flex flex-col items-center">
+        <h2 className="mt-10 mb-4 text-2xl text-gray-800">
+          Click here to navigate back to the home page
+        </h2>
+        <a className="px-4 py-2 blue button">Home</a>
+      </a>
+    </Link>
+  </section>
+  )
+}
